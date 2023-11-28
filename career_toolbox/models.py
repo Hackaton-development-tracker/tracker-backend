ca9381b7135a9be9ae14a4983660505898f951ad
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-11-28 16:54:10 +0200

    feat(model): Add models
    Added Project, ProjectMembership (link),
    Course, CourseEnrollment (link),
    Specialization, SpecializationRelation
    (link), Skill, SkillProficiency (link)
    and User models to represent data about
    projects, courses, specializations and
    user skills.

--- career_toolbox/models.py
+++ career_toolbox/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    pass
+
+
+class ProjectMembership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+
+class Course(models.Model):
+    pass
+
+
+class CourseEnrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+
+class Specializtion(models.Model):
+    pass
+
+
+class SpecializationRelation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+
+class Skill(models.Model):
+    pass
+
+
+class SkillProficiency(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    pass
