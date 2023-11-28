2b2ba5701893ec1101ceb6deed3bf9a9791c8c7f
Форов Александр <5h9t5@list.ru>
2023-11-28 20:11:31 +0300

    feat(users): Переопределил базовый менеджер моделей, добавил кастомную модель юзера, добавил модель юзера в админку.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email'
+    )
+    list_filter = ('is_active', )
+    search_fields = ('email', )
+    ordering = ('id',)
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,63 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    """
+    Определиние базового менеджера моделей для пользовательской модели.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Создание и сохранение пользователя с указанным адресом электронной
+        почты и паролем.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Создание и сохранение суперпользователя с указанным адресом электронной
+        почты и паролем.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """
+    Кастомная модель пользователя.
+    """
+    username = None
+    email = models.EmailField(
+        'Почта',
+        max_length=150,
+        blank=False,
+        unique=True
+    )
+    first_name = models.CharField('Имя', max_length=150, blank=False)
+    last_name = models.CharField('Фамилия', max_length=150, blank=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = MyUserManager()
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ['id', ]
+
+    def __str__(self):
+        return self.email
