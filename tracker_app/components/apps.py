f4098381f79e9a59188b0082b77a751ca2f143e8
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-11-28 16:57:27 +0200

    feat(tracker_app/components): add rest_framework and quiz app
    Added 'rest_framework' and 'quiz' application (testing
    functionality) to the project config

--- tracker_app/components/apps.py
+++ tracker_app/components/apps.py
@@ -3,7 +3,9 @@
 from tracker_app.components.common import INSTALLED_APPS
 
 INSTALLED_APPS += (
+    'rest_framework',
     'api.apps.ApiConfig',
     'career_toolbox.apps.CareerToolboxConfig',
     'users.apps.UsersConfig',
+    'quiz.apps.QuizConfig',
 )
