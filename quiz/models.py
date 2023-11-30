19083de33310c903a6a37f4739488f63c63f3422
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-11-30 18:55:45 +0200

    feat(quiz): добавить новую модель
    Добавлена новая модель в файл quiz/models.py:
    - Test

--- quiz/models.py
+++ quiz/models.py
@@ -2,4 +2,24 @@ from django.db import models
 
 
 class Test(models.Model):
-    pass
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Название теста'
+    )
+    test_content = models.TextField(
+        verbose_name='Тест'
+    )
+    skills = models.OneToOneField(
+        'career_toolbox.Skill',
+        on_delete=models.SET_NULL,
+        verbose_name='Навык для тестирования',
+        related_name='test_skill',
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+
+    def __str__(self):
+        return self.title
