ab58751e2c92ae69b0f090bb90008c94164f1e54
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-12-05 17:45:04 +0200

    refactor: Изменить конфигурацию подключения к базе данных
    Обновлена конфигурация базы данных динамическм - в зависимости
    от значения переменной окружения ENVIRONMENT (тестирование или
    продакшн).

--- tracker_app/components/database.py
+++ tracker_app/components/database.py
@@ -4,22 +4,25 @@ from pathlib import Path
 
 from decouple import config
 
+ENVIRONMENT = config('ENVIRONMENT')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': '172.18.0.2',
-#         'PORT': config('DB_PORT', 5432),
-#     }
-# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if ENVIRONMENT == 'testing':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+elif ENVIRONMENT == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': 'localhost',
+            'PORT': config('DB_PORT', 5432),
+        }
+    }
