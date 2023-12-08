35f20551ad842d63196cfe85c6a41a4843749157
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-12-08 10:23:44 +0200

    refactor: Скорректировать настройки для админки моделей career_toolbox
    Изменены настройки для административных панелей моделей. Перебор в цикле
    и регистарция моделей заменен на декоратор к каждой модели.

--- career_toolbox/admin.py
+++ career_toolbox/admin.py
@@ -2,51 +2,44 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'start_date', 'end_date')
     search_fields = ('title',)
     list_filter = ('start_date', 'end_date')
 
 
+@admin.register(models.ExternalResource)
 class ExternalResourceAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'url')
     search_fields = ('title',)
 
 
+@admin.register(models.Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'start_date', 'end_date')
     search_fields = ('title',)
 
 
+@admin.register(models.Specialization)
 class SpecializationAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
     search_fields = ('title',)
 
 
+@admin.register(models.Grade)
 class GradeAdmin(admin.ModelAdmin):
     list_display = ('title', 'point')
     search_fields = ('title',)
 
 
+@admin.register(models.Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
     search_fields = ('title',)
 
 
+@admin.register(models.KnowledgeBase)
 class KnowledgeBaseAdmin(admin.ModelAdmin):
     list_display = ('theme', 'description', 'author')
     search_fields = ('theme',)
-
-
-model_admin_mapping = {
-    models.Project: ProjectAdmin,
-    models.ExternalResource: ExternalResourceAdmin,
-    models.Course: CourseAdmin,
-    models.Specialization: SpecializationAdmin,
-    models.Grade: GradeAdmin,
-    models.Skill: SkillAdmin,
-    models.KnowledgeBase: KnowledgeBaseAdmin,
-}
-
-for model, admin_class in model_admin_mapping.items():
-    admin.site.register(model, admin_class)
