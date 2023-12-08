716494655b16d1b248a36f0ca39d045a8189e049
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-12-08 09:53:44 +0200

    fix: Изменить модели в приложении quiz
    1. Удалена модель Test.
    
    2. Добавлена модель QuestionTest:
       - Добавлено поле question, тип поля TextField.
    
    3. Добавлена модель AnswerTest:
       - Добавлено поле answer, тип поля TextField.
       - Добавлено поле specializtion, связанное с моделью Specialization, OneToOneField.
       - Добавлено поле grade, связанное с моделью Grade, ForeignKey.
       - Добавлено поле point_answer, тип поля CharField с выбором вариантов ответов.
    
    4. Все поля, связанные с вопросами и ответами, были сделаны nullable и blank.

--- quiz/models.py
+++ quiz/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
 
-class Test(models.Model):
-    title = models.CharField(
-        max_length=255,
-        verbose_name='Название теста'
-    )
-    test_content = models.TextField(
-        verbose_name='Тест'
+class QuestionTest(models.Model):
+    question = models.TextField(
+        verbose_name='Вопрос'
     )
     skills = models.OneToOneField(
         'career_toolbox.Skill',
@@ -18,8 +14,53 @@ class Test(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Тест'
-        verbose_name_plural = 'Тесты'
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
 
     def __str__(self):
-        return self.title
+        return self.question
+
+
+class AnswerTest(models.Model):
+
+    class PointsAnswerChoices(models.TextChoices):
+        Variant_1 = 'V1', '0'
+        Variant_2 = 'V2', '3'
+        Variant_3 = 'V3', '1.5'
+        Variant_4 = 'V4', '1'
+        Variant_5 = 'V5', '0.75'
+
+    questions = models.ForeignKey(
+        'quiz.QuestionTest',
+        on_delete=models.CASCADE,
+        verbose_name='К вопросу',
+        related_name='answertest_questions'
+    )
+    answer = models.TextField(
+        verbose_name='Ответ'
+    )
+    specializtion = models.OneToOneField(
+        'career_toolbox.Specialization',
+        on_delete=models.CASCADE,
+        verbose_name='Связанная специализация',
+        related_name='answertest_specialization'
+    )
+    grade = models.ForeignKey(
+        'career_toolbox.Grade',
+        on_delete=models.CASCADE,
+        verbose_name='Связанный грейд',
+        related_name='answertest_grade'
+    )
+    point_answer = models.CharField(
+        max_length=3,
+        verbose_name='Баллы за ответ',
+        choices=PointsAnswerChoices.choices,
+        default=PointsAnswerChoices.Variant_1
+    )
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+    def __str__(self):
+        return self.answer
