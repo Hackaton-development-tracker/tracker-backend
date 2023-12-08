2a8d4c0bf646bf23404510da2e808bb7dde1f6fc
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-12-08 10:18:23 +0200

    chore: Настроить админ-панель для моделей QuestionTest и AnswerTest
    1. QuestionTestAdmin - вопросы теста
    2. AnswerTestAdmin - ответы на вопросы теста

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import AnswerTest, QuestionTest
 
-# Register your models here.
+
+@admin.register(QuestionTest)
+class QuestionTestAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'skills',
+    )
+    search_fields = ('question',)
+
+
+@admin.register(AnswerTest)
+class AnswerTestAdmin(admin.ModelAdmin):
+    list_display = (
+        'questions',
+        'answer',
+        'point_answer',
+        'specializtion',
+        'grade',
+    )
+    list_filter = ('questions',)
