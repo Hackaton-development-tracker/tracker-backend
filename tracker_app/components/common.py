a8e7d5cc878ee812807dc4f61e4985f7fda8cc93
Форов Александр <5h9t5@list.ru>
2023-12-10 23:40:01 +0300

    Установил настройки прав доступа в IsAuthenticated

--- tracker_app/components/common.py
+++ tracker_app/components/common.py
@@ -41,7 +41,7 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
